dd08f568d3629523442de53d3bc0550311a27446
albert <asimcs@gmail.com>
2017-06-27 13:30:28 +0800

    initial code

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace usb_speed_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usb_test_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.vidText1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pidText111 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usb_test_btn
+            // 
+            this.usb_test_btn.Location = new System.Drawing.Point(12, 3);
+            this.usb_test_btn.Name = "usb_test_btn";
+            this.usb_test_btn.Size = new System.Drawing.Size(75, 21);
+            this.usb_test_btn.TabIndex = 0;
+            this.usb_test_btn.Text = "Start";
+            this.usb_test_btn.UseVisualStyleBackColor = true;
+            this.usb_test_btn.Click += new System.EventHandler(this.usb_test_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Open File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(12, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current Speed";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "...";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1MBytes",
+            "5MBytes",
+            "50MBytes",
+            "100MBytes",
+            "300MBytes",
+            "1GBytes"});
+            this.comboBox2.Location = new System.Drawing.Point(264, 5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Allowed File Size";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "File Size";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox2.Location = new System.Drawing.Point(264, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "MBytes";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(762, 293);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Repeat";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(62, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            360000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vidText1
+            // 
+            this.vidText1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.vidText1.Location = new System.Drawing.Point(542, 32);
+            this.vidText1.Name = "vidText1";
+            this.vidText1.Size = new System.Drawing.Size(121, 22);
+            this.vidText1.TabIndex = 15;
+            this.vidText1.Text = "13FE";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(542, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 16);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Update";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(669, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "PID";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(669, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 12);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "VID";
+            // 
+            // pidText111
+            // 
+            this.pidText111.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.pidText111.Location = new System.Drawing.Point(542, 60);
+            this.pidText111.Name = "pidText111";
+            this.pidText111.Size = new System.Drawing.Size(121, 22);
+            this.pidText111.TabIndex = 20;
+            this.pidText111.Text = "5500";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 415);
+            this.Controls.Add(this.pidText111);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.vidText1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.usb_test_btn);
+            this.Name = "Form1";
+            this.Text = "USB Speed Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private USBClassLibrary.USBClass USBPort;
+        private System.Collections.Generic.List<USBClassLibrary.USBClass.DeviceProperties> ListOfUSBDeviceProperties;
+        bool MyUSBDeviceConnected = false;
+        private const uint MyDeviceVID = 0X13FE; //16Gb sandisk
+        private const uint MyDevicePID = 0X5500; //16Gb sandisk
+
+
+        private System.Windows.Forms.Button usb_test_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox vidText1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox pidText111;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using USBClassLibrary;
+
+namespace usb_speed_test
+{    
+    public partial class Form1 : Form
+    {
+        int buffer_len = 50*1024*1024; //50Mbytes
+
+
+        Nullable<UInt32> MI = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //USB Connection
+            MI = null;
+
+            USBPort = new USBClass();
+            ListOfUSBDeviceProperties = new List<USBClass.DeviceProperties>();
+            USBPort.USBDeviceAttached += new USBClass.USBDeviceEventHandler(USBPort_USBDeviceAttached);
+            USBPort.USBDeviceRemoved += new USBClass.USBDeviceEventHandler(USBPort_USBDeviceRemoved);
+            USBPort.RegisterForDeviceChange(true, this.Handle);
+            USBTryMyDeviceConnection();
+            MyUSBDeviceConnected = false;
+        }
+        private bool USBTryMyDeviceConnection()
+        {            
+            if (USBClass.GetUSBDevice(uint.Parse(vidText1.Text, System.Globalization.NumberStyles.AllowHexSpecifier), uint.Parse(pidText111.Text, System.Globalization.NumberStyles.AllowHexSpecifier), ref ListOfUSBDeviceProperties, false, MI))
+            {
+                //My Device is attached
+                richTextBox1.AppendText("Number of found devices: " + ListOfUSBDeviceProperties.Count.ToString() + "\n");
+                
+                
+                for (int i = 0; i < ListOfUSBDeviceProperties.Count; i++)
+                {
+                    richTextBox1.AppendText("Device " + i.ToString());
+                }                
+                Connect();
+
+                return true;
+            }
+            else
+            {
+                Disconnect();
+                return false;
+            }
+        }
+
+        private void USBPort_USBDeviceAttached(object sender, USBClass.USBDeviceEventArgs e)
+        {
+            if (!MyUSBDeviceConnected)
+            {
+                if (USBTryMyDeviceConnection())
+                {
+                    MyUSBDeviceConnected = true;
+                }
+            }
+        }
+
+        private void USBPort_USBDeviceRemoved(object sender, USBClass.USBDeviceEventArgs e)
+        {
+            if (!USBClass.GetUSBDevice(MyDeviceVID, MyDevicePID, ref ListOfUSBDeviceProperties, false))
+            {
+                //My Device is removed
+                MyUSBDeviceConnected = false;
+                Disconnect();
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            bool IsHandled = false;
+
+            USBPort.ProcessWindowsMessage(m.Msg, m.WParam, m.LParam, ref IsHandled);
+
+            base.WndProc(ref m);
+        }
+
+        private void Connect()
+        {
+            //TO DO: Insert your connection code here
+            MessageBox.Show("Connected!");
+            //ConnectionToolStripStatusLabel.Text = "Connected";
+        }
+
+        private void Disconnect()
+        {
+            //TO DO: Insert your disconnection code here
+            MessageBox.Show("Disconnected!");
+            //ConnectionToolStripStatusLabel.Text = "Disconnected";
+            //InitializeDeviceTextBoxes();
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = 4;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void usb_test_btn_Click(object sender, EventArgs e)
+        {
+            usb_test_btn.Text = "Stop";
+            usb_test_btn.Enabled = false;
+            long length = 0;
+            if (textBox1.Text == "") {
+                MessageBox.Show("Open File Failed");
+                usb_test_btn.Text = "Start";
+                return;
+            }
+                
+            try {
+                length = (int)(new System.IO.FileInfo(textBox1.Text).Length);
+            }
+            catch(IOException e1)
+            {
+                MessageBox.Show("Open File Failed");
+                usb_test_btn.Text = "Start";
+                usb_test_btn.Enabled = true;
+                return;
+            }
+            textBox2.Text = (length/(1024*1024)).ToString();
+            if (length > buffer_len)
+            {
+                MessageBox.Show("Selected file size is over limitation");
+                usb_test_btn.Text = "Start";
+                usb_test_btn.Enabled = true;
+                return;
+            }
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                byte[] array = new byte[length];
+                const FileOptions FileFlagNoBuffering = (FileOptions)0x20000000;
+                FileStream infile = new FileStream(openFileDialog1.FileName,
+                                    FileMode.Open, FileAccess.Read, FileShare.None, 8
+                                , FileFlagNoBuffering | FileOptions.SequentialScan);
+                long x = length / 512;
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                //byte[] array = File.ReadAllBytes(openFileDialog1.FileName);
+
+                //no cach file read                
+                infile.Read(array, 0, (int)x*512);                
+                sw.Stop();
+                infile.Close();
+                infile.Dispose();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                long elapsed_time = sw.ElapsedMilliseconds > 1 ? sw.ElapsedMilliseconds : 1;
+                double speed = ((length * 1000) / elapsed_time) / (1024 * 1024);
+                label2.Text = DateTime.Now + " " + speed + "MBytes";
+                this.Refresh();
+                richTextBox1.AppendText(Environment.NewLine + DateTime.Now + "Speed: " + speed + " MBytes/second" + ", File: " + textBox1.Text + ", size: " + textBox2.Text + "MBytes, Elapsed Time: " + sw.Elapsed);
+                Thread.Sleep(1);
+                
+            }
+            usb_test_btn.Text = "Start";
+            usb_test_btn.Enabled = true;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //1MBytes 
+            //5MBytes
+            //50MBytes (default)
+            //100MBytes
+            //300MBytes
+            //1GBytes            
+
+            if (comboBox2.SelectedIndex == 0)
+                buffer_len = 1 * 1024 * 1024;
+            else if (comboBox2.SelectedIndex == 1)
+                buffer_len = 5 * 1024 * 1024;
+            else if (comboBox2.SelectedIndex == 2)
+                buffer_len = 50 * 1024 * 1024;
+            else if (comboBox2.SelectedIndex == 3)
+                buffer_len = 100 * 1024 * 1024;
+            else if (comboBox2.SelectedIndex == 4)
+                buffer_len = 300 * 1024 * 1024;
+            else if (comboBox2.SelectedIndex == 5)
+                buffer_len = 1024 * 1024 * 1024;
+            else { }            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            USBTryMyDeviceConnection();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vidText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
